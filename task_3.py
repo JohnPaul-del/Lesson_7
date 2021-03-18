2550e630af4631d0f45d1f15aed4f44c2059fcfc
ivan <ivan.pavlov.alk@gmail.com>
2021-03-18 16:49:23 +0200

    Task 1 - Complete Task 3 - in progress

--- task_3.py
+++ task_3.py
@@ -1,5 +1,8 @@
 import shutil
 import os
 
-
-
+for main_dir, dirs, files in os.walk("my_project"):
+    for s_file in files:
+        if s_file.endswith(".html"):
+            print(os.path.abspath(s_file))
+#          shutil.copytree(main_dir, os.path.join(os.path.dirname(main_dir), r"templates"))
